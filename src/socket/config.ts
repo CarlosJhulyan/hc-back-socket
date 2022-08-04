3a40e7f15013930080c97a6341a308bbf0ebddf3
David Almeyda <dav.almeyda@gmail.com>
2022-08-04 00:50:46 -0500

    reservas

--- src/socket/config.ts
+++ src/socket/config.ts
@@ -1,6 +1,6 @@
 import { Server } from 'socket.io';
 import { notificaciones } from './notificaciones';
-import { reservaTeporal } from './reservaTemporal';
+import { eliminarReservasVencidas, enviarDatainicio, reservaTeporal } from './reservaTemporal';
 
 export function initialSocket(app) {
 	const server = app.getHttpServer();
@@ -10,6 +10,14 @@ export function initialSocket(app) {
 			methods: ['GET', 'POST'],
 		},
 	});
+
+	setInterval(() => {
+		eliminarReservasVencidas(io);
+	}, 5000);
+
+	setTimeout(() => {
+		enviarDatainicio(io);
+	}, 2000);
 
 	io.on('connection', socket => {
 		console.log('Conexión', socket.id, 'desde:', socket.handshake.headers.origin);
--- src/socket/reservaTemporal.ts
+++ src/socket/reservaTemporal.ts
@@ -1,9 +1,34 @@
 import { Server, Socket } from 'socket.io';
 
+const segundosReserva = 20;
 const reservas: Map<string, any>[] = [];
+let reservasEliminadas = false;
+
+export function enviarDatainicio(io: Server) {
+	io.emit('reservas', reservas);
+	console.log('Enviando reservas inicio');
+}
+
+// ELIMINAR RESERVAS QUE HAYAN PASADO LOS SEGUNDOS DE RESERVA EN SEGUNDOSRESERVA
+export function eliminarReservasVencidas(io: Server) {
+	reservas.forEach(reserva => {
+		const ff = new Date(reserva['horaRegistro']);
+		console.log(ff);
+		if (ff.getTime() + segundosReserva * 1000 < new Date().getTime()) {
+			reservas.splice(reservas.indexOf(reserva), 1);
+			reservasEliminadas = true;
+			console.log('Reserva eliminada', reservas);
+		}
+		if (reservasEliminadas) {
+			console.log('notificado');
+			io.emit('reservas', reservas);
+			reservasEliminadas = false;
+		}
+	});
+}
 
 export function reservaTeporal(io: Server, socket: Socket) {
-	socket.on('reservaTemporal', data => {
+	socket.on('agregarReserva', data => {
 		if (
 			!reservas.find(
 				reserva =>
@@ -15,11 +40,13 @@ export function reservaTeporal(io: Server, socket: Socket) {
 			)
 		) {
 			reservas.push(data);
+			io.emit('reservas', reservas);
 		}
 		console.log('reservas', reservas);
 	});
 
 	socket.on('quitarReserva', data => {
+		console.log('quitarReserva', data);
 		const index = reservas.findIndex(
 			reserva =>
 				reserva['hora'] === data.hora &&
@@ -30,6 +57,7 @@ export function reservaTeporal(io: Server, socket: Socket) {
 		);
 		if (index !== -1) {
 			reservas.splice(index, 1);
+			io.emit('reservas', reservas);
 		}
 		console.log('reservas', reservas);
 	});
